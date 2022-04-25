1bc2c1fb5668331a7ec6f0d2135701c887d46be1
kenai2020 <mauricioquirozsalazar09@gmail.com>
2022-04-25 12:03:55 -0400

    listas enlazadas

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import lcse.ListaCircularSimplementeEnlazada;
+import lse.ListasDoblementeEnlazada;
+import lse.ListaSimplementeEnlazada;
+public class Main {
+    public static void main(String[] args) {
+
+       // ListaSimplementeEnlazada listaSimplementeEnlazada = new ListaSimplementeEnlazada();
+       // System.out.println(listaSimplementeEnlazada.estaVacia());
+
+        //insertar elementos a nuestra lista
+       // listaSimplementeEnlazada.insertar(5);
+       // listaSimplementeEnlazada.insertar(3);
+       //listaSimplementeEnlazada.insertar(2);
+        //listaSimplementeEnlazada.insertar(1);
+        //listaSimplementeEnlazada.insertar(0);
+
+
+        //mostrar nuestra lista
+       // listaSimplementeEnlazada.mostrar();
+
+        //eliminar algunos elementos
+       // listaSimplementeEnlazada.eliminar();
+       // listaSimplementeEnlazada.eliminar();
+
+        //mostrar nuestra lista
+       // listaSimplementeEnlazada.mostrar();
+
+
+
+
+
+        // ListasDoblementeEnlazada listasDoblementeEnlazada = new ListasDoblementeEnlazada();
+        // System.out.println(listasDoblementeEnlazada.estaVacia());
+
+        //  for (int i = 20; i > 0; i--){
+        //     listasDoblementeEnlazada.insertar(i);
+        //  }
+
+        // System.out.println(listasDoblementeEnlazada.estaVacia());
+        // listasDoblementeEnlazada.mostrarAdelante();
+
+        //  listasDoblementeEnlazada.eliminar();
+        //  listasDoblementeEnlazada.eliminar();
+
+        // listasDoblementeEnlazada.mostrarAdelante();
+        //  listasDoblementeEnlazada.mostrarAtras();
+
+
+
+        
+
+        ListaCircularSimplementeEnlazada listaCircularSimplementeEnlazada = new ListaCircularSimplementeEnlazada();
+        System.out.println(listaCircularSimplementeEnlazada.estaVacia());
+        System.out.println(listaCircularSimplementeEnlazada.tamaño());
+
+        for (int i = 1; i <= 12; i++) {
+            listaCircularSimplementeEnlazada.insertarFin(i);
+
+        }
+
+        listaCircularSimplementeEnlazada.mostrar();
+        System.out.println(listaCircularSimplementeEnlazada.tamaño());
+
+       // listaCircularSimplementeEnlazada.eliminarFin();
+       // listaCircularSimplementeEnlazada.eliminarFin();
+
+       // listaCircularSimplementeEnlazada.mostrar();
+
+
+        listaCircularSimplementeEnlazada.mostrarCircular(2);
+
+    }
+}
+
